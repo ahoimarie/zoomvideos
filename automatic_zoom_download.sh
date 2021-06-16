f085d51cb51a846dfa432d55b621569ab90f0b43
ahoimarie <marie@kent.nu>
2021-06-16 15:55:57 +0200

    changed jq and curl path to variables

--- automatic_zoom_download.sh
+++ automatic_zoom_download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+<<<<<<< HEAD
 # fill in the variables: YOURTOKEN, /path/to/, SPECIALFOLDER, meetingID
 
 cd ~/Downloads/myzoomvideos
@@ -36,6 +37,43 @@ done
 
 mv *SPECIAL*.MP4 SPECIALFOLDER/vid 2>/dev/null;
 mv *SPECIAL* SPECIALFOLDER 2>/dev/null;
+=======
+
+cd ~/Downloads/myzoomvideos
+
+export TOKEN=YOURTOKEN # Add your Token
+export authorization="Authorization: Bearer $TOKEN"
+export meetingID=12345678 #Change to your meetingID
+export CURLPATH=$(which curl)
+export JQPATH=$(which jq)
+
+my_url=$($CURLPATH --header "$authorization" \
+ --url "https://api.zoom.us/v2/meetings/$meetingID/recordings" | $JQPATH -r '[.recording_files[]?.download_url] | join(",") ')
+IFS=',' read -ra myurls <<< "$my_url"
+
+mynames=$($CURLPATH --header "$authorization" \
+--url "https://api.zoom.us/v2/meetings/$meetingID/recordings" | \
+$JQPATH -r ['.topic as $parent | .recording_files[]? | "\(.recording_start)_\($parent[0:12]).\(.file_type)"] | join(",")')
+IFS=',' read -ra mynamevar <<< "$mynames"
+
+for i in $(echo ${!mynamevar[@]}); do \
+$CURLPATH -L -v --output "${mynamevar[$i]}" --url "${myurls[$i]}?access_token=$TOKEN";
+done
+
+## DELETE the videos already downloaded from Zoom##
+
+recID=$(curl --header "$authorization" \
+--url "https://api.zoom.us/v2/meetings/$meetingID/recordings" | \
+$JQPATH -r ['.recording_files[]?.id] | join(",")')
+IFS=',' read -ra recID <<< "$recID"
+
+for i in "${recID[@]}"; do \
+$CURLPATH -X DELETE --header "$authorization" --url https://api.zoom.us/v2/meetings/$meetingID/recordings/${i}?action=trash
+done
+
+mv -i *SPECIAL*.MP4 SPECIALFOLDER/vid 2>/dev/null; # change SPECIAL and SPECIALFOLDER according to your desired sorting strucure
+mv -i *SPECIAL* SPECIALFOLDER/ 2>/dev/null;
+>>>>>>> b81a075 (changed jq and curl path to variables)
 mv 202* OTHERS 2>/dev/null;
 
 echo "Download and deletion successful."
